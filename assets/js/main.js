d7a18a71decfd82904dfae679c770812f18d1503
Andrea <dauriaandrea99@gmail.com>
2020-11-19 17:22:39 +0100

    ! END !

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// FizzBuzz
+for (var i=1; i<101;i++) {
+    if (i % 3 == 0 && i % 5 == 0 ) {
+       document.getElementById('list').innerHTML += '<li>' + 'FizzBuzz' + '</li>';
+    } else if (i % 5 == 0) {
+        document.getElementById('list').innerHTML += '<li>' + 'Fizz' + '</li>';
+    } else if (i % 3 == 0) {
+        document.getElementById('list').innerHTML += '<li>' + 'Buzz' + '</li>';
+    } else {
+        document.getElementById('list').innerHTML += '<li>' + i + '</li>';
+    }
+}
